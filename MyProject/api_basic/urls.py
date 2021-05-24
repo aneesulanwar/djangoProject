28f8206f4e82b5829561af824f040a0cf0e696fc
aneesulanwar <syedanees06@gmail.com>
2021-05-24 12:51:40 +0500

    rest_frame....

--- MyProject/api_basic/urls.py
+++ MyProject/api_basic/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import articles
+from .views import articles,sportArticles,articlesDetails, sportsDetails
 
 urlpatterns = [
     path('articles/', articles),
+    path('sport-articles/', sportArticles),
+    path('articles-details/<str:title>/',articlesDetails),
+    path('sports-details/<str:title>/',sportsDetails)
 ]
--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -1,25 +1,98 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Article,sportArticle
 from .serializer import ArticleSerializer, SportArticleSerializer
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
+@api_view(['GET','POST'])
 def articles(request):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles,many=True)
-        return JsonResponse(serializer.data,safe=False)
+        return Response(serializer.data)
 
     if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=data)
+        
+        serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','POST'])
+def sportArticles(request):
+    if request.method == 'GET':
+        articles = sportArticle.objects.all()
+        serializer = SportArticleSerializer(articles,many=True)
+        return Response(serializer.data,safe=False)
+
+    if request.method == 'POST':
+    
+        serializer = SportArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET','PUT','DELETE'])
+def articlesDetails(request,title):
+    try:
+        article = Article.objects.get(title=title)
+    except Article.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        
+        serializer = ArticleSerializer(article,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "Delete":
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@csrf_exempt
+def sportsDetails(request,title):
+    try:
+        article = sportArticle.objects.get(title=title)
+    except sportArticle.DoesNotExist:
+        return HttpResponse(status=404)
+    
+
+    if request.method == 'GET':
+        serializer = SportArticleSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SportArticleSerializer(article,data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=201)
         else:
             return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == "Delete":
+        article.delete()
+        return HttpResponse(status=204)
+
+
 
 
 # Create your views here.
